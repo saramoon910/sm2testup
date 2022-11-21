d4a83269acfa0d082bae7330df9d03481bf55e98
saramoon910 <118172988+saramoon910@users.noreply.github.com>
2022-11-21 18:56:58 +0900

    1

---
+++ temp.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+import streamlit as st
+
+import numpy as np
+from mtcnn import MTCNN
+from PIL import Image, ImageDraw
+
+st.title("Pythonで顔認識してマスクを付けさせる")
+
+# 画像ファイルをアップロードするためのウィジェット
+imgfile = st.file_uploader("Upload Image", type=["png", "jpg"], accept_multiple_files=False)
+
+# 画像ファイルがアップロードされていなければ何もしない
+if imgfile is not None:    
+    img = Image.open(imgfile)
+
+    # マスクのイラストの画像
+    mask = Image.open("mask.png")
+
+    # 元の画像を表示
+    st.write("元の画像")
+    st.image(img, use_column_width=True)
+
+    # 画像に写っている顔を検出する
+    detector = MTCNN()  
+    # 検出された顔ごとに，顔のBounding Box，顔である確率，目や鼻などのKeypointsが得られる． 
+    results = detector.detect_faces(np.asarray(img))
+
+    for result in results:
+        # 顔である確率
+        confidence = result["confidence"]
+
+        # 顔である確率が90%以下ならスルー
+        if confidence < 0.9:
+            continue
+
+        # 顔のBounding Box
+        x, y, w, h = result["box"]
+
+        # マスクの画像を顔のサイズに合わせる．
+        mask_resized = mask.resize((w, h//2)) 
+
+        # マスクの画像を，検出された顔に貼り付ける．       
+        img.paste(mask_resized, (x, y+h//2), mask_resized.convert("RGBA"))
+
+    pil_img = Image.fromarray(np.uint8(img))
+
+    st.write("マスクを付けた画像")
+    st.image(pil_img, use_column_width=True)
